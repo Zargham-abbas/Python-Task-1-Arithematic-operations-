576ae3359d08c7d95cab2c26c4fcea426802829e
Zargham Abbas <zarghamabbas859@gmail.com>
2024-03-13 00:23:01 +0500

    Add files via upload

---
+++ division.py
@@ -0,0 +1,17 @@
+# Zargham-abbas
+# zarghamabbas859@gmail.com
+
+# Question No. 4:
+# Write a Python program to do arithmetical operation Division.
+
+# Declare a variable with integer datatype "a" and "b":
+a=int(input("enter a number :"));
+b=int(input("enter a number :"));
+
+# Declare division in order to divide "a" and "b":
+division=a/b;
+
+# Print division answer:
+print('a/b=',division);
+
+# DONE!
